6587be964eee5cf99bb38f4553a409c3cbde59ff
liru <17600929363@163.com>
2018-04-26 10:01:21 +0800

    不良事件

---
+++ src/view/adverse-event/pending-approval.vue
@@ -0,0 +1,39 @@
+<style>
+</style>
+<template>
+  <div class="txt cont">
+    <head-con :headData="headData"></head-con>
+    <div class="main">
+      <img-list :imgData="imgData"></img-list>
+    </div>
+  </div>
+</template>
+<script>
+  import headCon from '@/components/head.vue'
+  import imgList from '@/components/img_list.vue'
+  import headData from '@/data/adverse-event-title.json'
+  import rotateIcon from '../../../static/image/icon/rotate.png'
+  import searchInput from '../../../static/image/adverse-event/pending-approval1.png'
+  import searchInputOne from '../../../static/image/adverse-event/pending-approval2.png'
+
+
+  let imgData=[
+    {
+      headImg:rotateIcon,
+      headTitle:"上报不良事件",
+      img:[searchInput,searchInputOne]
+    }
+  ]
+  export default {
+    data () {
+      return {
+        headData,
+        imgData,
+      }
+    },
+    components:{
+      headCon,
+      imgList,
+    }
+  }
+</script>
---
+++ src/view/adverse-event/statistical-chart.vue
@@ -0,0 +1,39 @@
+<style>
+</style>
+<template>
+  <div class="txt cont">
+    <head-con :headData="headData"></head-con>
+    <div class="main">
+      <img-list :imgData="imgData"></img-list>
+    </div>
+  </div>
+</template>
+<script>
+  import headCon from '@/components/head.vue'
+  import imgList from '@/components/img_list.vue'
+  import headData from '@/data/adverse-event-title.json'
+  import rotateIcon from '../../../static/image/icon/rotate.png'
+  import searchInput from '../../../static/image/adverse-event/statistical-chart.png'
+
+
+
+  let imgData=[
+    {
+      headImg:rotateIcon,
+      headTitle:"医疗不良事件汇总表",
+      img:[searchInput]
+    }
+  ]
+  export default {
+    data () {
+      return {
+        headData,
+        imgData,
+      }
+    },
+    components:{
+      headCon,
+      imgList,
+    }
+  }
+</script>
