191f4f9798480d52ca573b4ae66d7704672410dd
guo <1012266847@qq.com>
2018-06-12 09:53:42 +0800

    '......'

---
+++ src/components/new-img-list.vue
@@ -0,0 +1,65 @@
+<style>
+.img-bg{
+  padding-top:15px;
+  padding-bottom:10px;
+  background:rgba(0,0,0,.3);
+}
+.txt .main .subTitle{
+  padding-top:3px;
+}
+  .txt .main .subTitle p{
+    font-size:14px;
+     line-height:14px;
+    margin-bottom:3px;
+  }
+.txt .main .subTitle p:nth-child(2){
+     font-size:12px;
+     line-height:12px;
+}
+</style>
+<template>
+  <div class="wrapper">
+    <div>
+      <div v-for="item in imgData">
+        <div class="title-con">
+          <div>
+            <img src="" alt="" v-lazy="item.headImg">
+          </div>
+          <div :class="{subTitle:!!item.headSubTitle}">
+            <p>{{item.headTitle}}</p>
+            <p>{{item.headSubTitle}}</p>
+          </div>
+        </div>
+        <div class="img-one img-bg" v-for="img in item.img">
+          <div>
+              <img src="" alt="" v-lazy="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    props:['imgData'],
+    methods:{
+      async initData(){
+        this.$nextTick(()=>{
+          new BScroll('.wrapper',{
+            scrollY:true,
+            click:true,
+          })
+        })
+      }
+    },
+    mounted(){
+      this.initData()
+    }
+  }
+</script>
---
+++ src/view/cockpit/analysis.vue
@@ -0,0 +1,42 @@
+<style>
+</style>
+<template>
+  <div class="txt cont">
+    <head-con :headData="headData"></head-con>
+    <div class="main">
+      <img-list :imgData="imgData"></img-list>
+    </div>
+  </div>
+</template>
+<script>
+  import headCon from '@/components/head.vue'
+  import headData from '@/data/medical-head-title.json'
+  import rotateIcon from '../../../static/image/icon/rotate.png'
+  import searchInput from '../../../static/image/cockpit/operational performance/1.1.png'
+  import searchInputOne from '../../../static/image/cockpit/operational performance/1.2.png'
+  import imgList from '@/components/new-img-list.vue'
+  let imgData=[
+    {
+      headImg:rotateIcon,
+      headTitle:"运营分析导航",
+      img:[searchInput]
+    },
+    {
+      headImg:rotateIcon,
+      headTitle:"运管助理分析导航",
+      img:[searchInputOne]
+    }
+  ]
+  export default {
+    data () {
+      return {
+        headData,
+        imgData,
+      }
+    },
+    components:{
+      headCon,
+      imgList,
+    }
+  }
+</script>
